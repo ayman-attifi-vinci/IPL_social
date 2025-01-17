371164f89bab2231ca743485a1ce9460ea38385f
attifi <ayman.attifi@student.vinci.be>
2025-01-17 17:06:42 +0100

    ajout test 4 chiffre patch

--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,7 +1,9 @@
 export default function isValidPassword(password) {
   const minLength = 8;
+  const hasNumber = /\d/.test(password);
   const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
   if (password.length < minLength) return false;
   if (!hasSpecialChar) return false;
+  if (!hasNumber) return false;
   return true;
 }
